5f79a4b0401a1a9804b9ca235bac25b3646c0377
Jess Jacobson <jessanova@gmail.com>
2018-10-17 14:20:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,33 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS=[
+  [0,1,2],
+  [0,4,8],
+  [0,3,6],
+  [1,4,7],
+  [3,4,5],
+  [2,4,6],
+  [2,5,8],
+  [6,7,8]
+  ]
+  
+def won?
+  
+end #won
+
+def full?
+  
+end #full
+
+def draw?
+  
+end #draw
+
+def over?
+  
+end #over
+
+def winner
+  
+end #winner
